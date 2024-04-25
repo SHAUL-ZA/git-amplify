94803449d59d7a0ebde87b654621d5e37d939139
Shaul-Za <shaulzayat1@gmail.com>
2024-04-25 16:46:10 -0600

    Feat: New branch

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        This is a shaul's branch of the vite-react template. <br />
       </p>
     </>
   )
